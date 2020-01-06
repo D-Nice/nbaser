aa2665bf5ad2bcd8a7fc0b7b25a0b7050ea67860
D-Nice <d-nice2@protonmail.com>
2020-01-05 21:11:23 -0500

    chore: update and move converters into utils

--- tests/utils.nim
+++ tests/utils.nim
@@ -1,1 +1,25 @@
-converter toSeqByte*(s: string): seq[byte] = cast[seq[byte]](s)
+import
+  sequtils,
+  strutils
+
+converter castToSeqByte*(s: string): seq[byte] = cast[seq[byte]](s)
+
+# convert byteSeq to hexString
+func toHex*(ba: openArray[byte]): string =
+  result = foldl(ba, a & b.toHex, "")
+
+# convert string char to byte
+func toByte*(s: string, i: int): byte =
+  const hexMap = "0123456789abcdef"
+  result = (hexMap.find(s[i]).shl(4) + hexMap.find(s[i + 1])).byte
+
+# convert hex string to byte sequence
+func fromHexToBytes*(src: string): seq[byte] =
+  var s = src
+  if s.len mod 2 != 0:
+    s.insert "0"
+
+  result = newSeq[byte](int s.len / 2)
+
+  for i in 0..result.high:
+    result[i] = s.toByte(i * 2)
