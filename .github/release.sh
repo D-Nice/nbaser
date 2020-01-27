2745c01326a794fadbe94a8135ac0fc5bb797dc5
D-Nice <d-nice2@protonmail.com>
2020-01-26 21:01:43 -0500

    chore: update release script

--- .github/release.sh
+++ .github/release.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-npx standard-version@7.1.0 --header '# CHANGELOG
+FIRST=""
+if [ ! -f CHANGELOG.md ]; then
+  FIRST="-f"
+fi
+npx standard-version@7.1.0 "$FIRST" --header '# CHANGELOG
 ' \
   && echo "Sign-off commit and tags via 'git commit --amend --signoff' and 'git tag -sf v...'"
